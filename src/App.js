8e290ed986a84b94313760674add19bfa932b474
lanmeidada <lanmeidada@qq.com>
2022-03-22 14:20:25 +0800

    添加Link导航

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+      <Link to="/invoices"> Invoices</Link> |{""}
+      <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
